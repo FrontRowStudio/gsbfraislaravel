76431f914459c0ecde9fab4910ce50202bda6ef0
anthony.bettollo <anthony.bettollo@SIO-SLAM-1.BtsSio-Carriat.com>
2017-12-12 17:49:17 +0100

    Lot 1 fin du formulaire créer un utilisateur + patern non finis + masterblade OK

--- app/metier/GsbFrais.php
+++ app/metier/GsbFrais.php
@@ -286,4 +286,16 @@ public function getInfosVisiteur($login, $mdp){
             $req ="INSERT INTO visiteur values(:id,:nom,:prenom,:login,:mdp,:adresse,:telephone,:adresseMail,:cp,:ville,:dateEmbauche,:statut)";
             DB::insert($req,['id'=>$id,'nom'=>$nom,'prenom'=>$prenom,'login'=>$login,'mdp'=>$mdp,'adresse'=>$adresse,'telephone'=>$telephone,'adresseMail'=>$adresseMail,'cp'=>$cp,'ville'=>$ville,'dateEmbauche'=>$dateEmbauche,':statut'=>$statut]);
     }
+    function Genere_Password($size)
+{
+    // Initialisation des caractères utilisables
+    $characters = array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+    $password = 0;
+    for($i=0;$i<$size;$i++)
+    {
+        $password .= ($i%2) ? strtoupper($characters[array_rand($characters)]) : $characters[array_rand($characters)];
+    }
+		
+    return strtolower($password);
+}
 }
