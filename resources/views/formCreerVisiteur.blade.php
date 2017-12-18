8fb688fac613452cd4b4065a163155d7a5204c63
anthony.bettollo <anthony.bettollo@SIO-SLAM-1.BtsSio-Carriat.com>
2017-12-18 14:04:17 +0100

    Lot 1 fin du formulaire créer un utilisateur + patern non finis + masterblade OK

--- resources/views/formCreerVisiteur.blade.php
+++ resources/views/formCreerVisiteur.blade.php
@@ -7,57 +7,57 @@
         <div class="form-group">
             <label class="col-md-2 control-label">Nom : </label>
             <div class="col-md-6 col-md-3">
-                <input type="text" name="nom" ng-model="nom" class="form-control" placeholder="Votre Nom" pattern="[a-zA-Z]{}" title="Saisir un nom composé de caractère alphabétique" >
+                <input type="text" name="nom" ng-model="nom" class="form-control" placeholder="Votre Nom" pattern="[a-zA-Z]{0,}" title="Saisir un nom composé de caractère alphabétique" required>
             </div>
             
             <div class="form-group">
             <label class="col-md-1 control-label">Prénom : </label>
             <div class="col-md-6 col-md-3">
-                <input type="text" name="prenom" ng-model="prenom" class="form-control" placeholder="Votre Prénom" pattern="[A-Za-z]{}" title="Saisir un prenom composé de caractère alphabétique" >
+                <input type="text" name="prenom" ng-model="prenom" class="form-control" placeholder="Votre Prénom" pattern="[A-Za-z]{0,}" title="Saisir un prenom composé de caractère alphabétique" required >
             </div>
         </div>
         </div>
         <div class="form-group">
             <label class="col-md-2 control-label">Adresse : </label>
             <div class="col-md-6 col-md-3">
-                <input type="text" name="adresse" ng-model="adresse" class="form-control" placeholder="Votre Adresse Postal" >
+                <input type="text" name="adresse" ng-model="adresse" class="form-control" placeholder="Votre Adresse Postal" required>
             </div>
             
             <div class="form-group">
             <label class="col-md-1 control-label">CP: </label>
             <div class="col-md-6 col-md-3">
-                <input type="text" name="cp" ng-model="cp" class="form-control" placeholder="Votre Code Postal" pattern="[0-9]{5}" title="Saisir un code postal composé de 5 chiffres"  >
+                <input type="text" name="cp" ng-model="cp" class="form-control" placeholder="Votre Code Postal" pattern="[0-9]{5}" title="Saisir un code postal composé de 5 chiffres" required >
             </div>
         </div>
         </div>
         <div class="form-group">
             <label class="col-md-2 control-label">Ville : </label>
             <div class="col-md-6 col-md-3">
-                <input type="text" name="ville" ng-model="ville" class="form-control" placeholder="Votre Ville" >
+                <input type="text" name="ville" ng-model="ville" class="form-control" placeholder="Votre Ville" required>
             </div>
             
             
             <label class="col-md-1 control-label">Adresse Mail : </label>
             <div class="col-md-6 col-md-3">
-                <input type="mail" name="mail" ng-model="mail" class="form-control" placeholder="Votre adresse mail"  >
+                <input type="mail" name="mail" ng-model="mail" class="form-control" placeholder="Votre adresse mail"  required>
             </div>
         </div>
              <div class="form-group">
             <label class="col-md-2 control-label">Téléphone : </label>
             <div class="col-md-6 col-md-3">
-                <input type="text" name="telephone" ng-model="telephone" class="form-control" placeholder="Votre Téléphone" >
+                <input type="text" name="telephone" ng-model="telephone" class="form-control" placeholder="Votre Téléphone" required>
             </div>
             
             
             <label class="col-md-1 control-label">Date Embauche: </label>
             <div class="col-md-6 col-md-3">
-                <input type="date" name="date" ng-model="date" class="form-control" placeholder="Votre adresse mail" >
+                <input type="date" name="date" ng-model="date" class="form-control" placeholder="Votre adresse mail" required>
             </div>
         </div>
         <div class="form-group">
             <label class="col-md-2 control-label">ID : </label>
             <div class="col-md-6 col-md-3">
-                <input type="text" name="id" ng-model="id" class="form-control" placeholder="ID" pattern="[A-Za-z0-9]{4}" title="Saisir un ID de 4 caractères" >
+                <input type="text" name="id" ng-model="id" class="form-control" placeholder="ID" pattern="[A-Za-z0-9]{4}" title="Saisir un ID de 4 caractères" required>
             </div>
             
             
